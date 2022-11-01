f7e6c9485295fa0a9ed59f075c7693a94716d02e
andressahcardoso <84184701+andressahcardoso@users.noreply.github.com>
2022-10-31 23:38:34 -0300

    Add files via upload

---
+++ Produtos/produtos.js
@@ -0,0 +1,39 @@
+var x = document.querySelector("#icone_menu_exit");
+var y = document.querySelector("#icone_menu");
+
+y.parentNode.replaceChild(y, x)
+
+var count = 1
+
+function menu_dropdown() {
+    console.log('oi')
+
+
+    if (count == 1) {
+        y.parentNode.replaceChild(x, y)
+        count = 2
+        console.log(1)
+    }
+
+    else {
+        x.parentNode.replaceChild(y, x)
+        count = 1
+        console.log(2)
+    }
+}
+
+
+var nome_relatorio = 'Produtos'
+
+function teste() {
+    var doc = new jsPDF()
+    doc.text('Tabela de Produtos', 10, 10)
+    doc.save(nome_relatorio+'.pdf')
+
+    var doc = new jsPDF({
+        orientation: 'landscape',
+        unit: 'cm',
+        format: 'letter'
+    })
+}
+
